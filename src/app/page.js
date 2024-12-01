84d8b2abc0b1df3782665b3dce610f4632e8bbb6
SahilV4re <vicharesahil14@gmail.com>
2024-12-01 13:31:40 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main>
       <h1>vaayudrishti</h1>
-      <Link href="/users">Local users</Link>
+      <Button><Link href="/users">Local users</Link></Button>
       <br></br>
-      <Link href="/researchpanel">researchers</Link>
+      <Button><Link href="/researchpanel">researchers</Link></Button>
       <br></br>
-      <Link href="/privatecompanies">private companies</Link>
+      <Button><Link href="/privatecompanies">private companies</Link></Button>
     </main>
     
   );
